0fbcb1df9ec6ac6a66f7469c8a3bee076afc8f03
drplote <drplote@gmail.com>
2020-05-28 09:20:15 -0400

    Fixing resistance with multiple damage types

--- scripts/manager_action_damage.lua
+++ scripts/manager_action_damage.lua
@@ -956,16 +956,49 @@ function checkReductionTypeHelper(rMatch, aDmgType)
   return true;
 end
 
+function hasBaseDamageTypes(aDmgTypes)
+	local bBludgeoning = false;
+	local bPiercing = false;
+	local bSlashing = false;
+	
+	for _,sDmgType in pairs(aDmgTypes) do
+		if sDmgType == "bludgeoning" then
+			bBludgeoning = true;
+		elseif sDmgType == "slashing" then
+			bSlashing = true;
+		elseif sDmgType == "piercing" then
+			bPiercing = true;
+		end
+	end
+	
+	return bBludgeoning, bPiercing, bSlashing;
+end
+
 function checkReductionType(aReduction, aDmgType)
-  if not aDmgType or #aDmgType == 0 then	
-	return false;
-  end
+   local aReducedDamageTypes = {};
+
+   for _,sDmgType in pairs(aDmgType) do
+	  if checkReductionTypeHelper(aReduction[sDmgType], aDmgType) or checkReductionTypeHelper(aReduction["all"], aDmgType) then
+	    table.insert(aReducedDamageTypes, sDmgType);
+      end
+   end  
   
-  local bReduced = true;
-  for _,sDmgType in pairs(aDmgType) do
-	bReduced = bReduced and (checkReductionTypeHelper(aReduction[sDmgType], aDmgType) or checkReductionTypeHelper(aReduction["all"], aDmgType));
-  end
-  return bReduced;
+   if #aReducedDamageTypes == 0 then
+	 return false;
+   else
+     -- We're going to treat bludgeoning/piercing/slashing differently, and say that if it contains more than one of these types, it must 
+	 -- resist ALL of these "base" damage types it contains to be resisted. (i.e., a bludgeoning/piercing morningstar shouldn't be resisted by 
+	 -- just bludgeoning resist, but should instead require both bludgeoning resist and piercing resist)
+     local bIsBludgeoning, bIsPiercing, bIsSlashing = hasBaseDamageTypes(aDmgType);
+	 local bResistsBludgeoning, bResistsPiercing, bResistsSlashing = hasBaseDamageTypes(aReducedDamageTypes);
+	 
+	 local bResistsAllBaseTypesItContains = true;
+	 bResistsAllBaseTypesItContains = bResistsAllBaseTypesItContains and (not bIsBludgeoning or bResistsBludgeoning);
+	 bResistsAllBaseTypesItContains = bResistsAllBaseTypesItContains and (not bIsPiercing or bResistsPiercing);
+	 bResistsAllBaseTypesItContains = bResistsAllBaseTypesItContains and (not bResistsSlashing or bResistsSlashing);
+
+	 return bResistsAllBaseTypesItContains;
+   end
 end
 
 function checkNumericalReductionTypeHelper(rMatch, aDmgType, nLimit)
